41f6d11dd42844758d818588b7e8eac974975aaa
mingu Kang <minqu.kang@gmail.com>
2025-02-16 01:21:05 +0900

    test

--- lib/home/home_detail_screen.dart
+++ lib/home/home_detail_screen.dart
@@ -21,7 +21,7 @@ class _HomeDetailScreenState extends State<HomeDetailScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return Scaffold(
       appBar: AppBar(
         title: const Text('detail'),
