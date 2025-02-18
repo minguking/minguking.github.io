45c921d412011707a28645aa8dde566e3572f547
M Kang <minqu.kang@gmail.com>
2025-02-18 17:55:54 +0900

    test in progress

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -26,33 +26,6 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   color: Colors.white,
-    //   child: Center(
-    //     child: ElevatedButton(
-    //       onPressed: () {
-    //         context.push('/home/detail');
-    //       },
-    //       child: const Text('Go to Detail'),
-    //     ),
-    //   ),
-    // );
-
-    // return DefaultTabController(
-    //   animationDuration: const Duration(milliseconds: 300),
-    //   length: 3,
-    //   child: Column(
-    //     children: [
-    //       // _pageMoveButton(),
-    //       _tabBar(),
-    //
-    //       /// Expanded 없으면 오류 발생
-    //       /// Horizontal viewport was given unbounded height.
-    //       Expanded(child: _tabBarView()),
-    //     ],
-    //   ),
-    // );
-
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -113,117 +86,25 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     );
   }
 
-  Widget _tabBar() {
-    return TabBar(
-      controller: tabController,
-      labelColor: Colors.black,
-      unselectedLabelColor: Colors.grey,
-      labelStyle: const TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-      ),
-      unselectedLabelStyle: const TextStyle(
-        fontSize: 16,
-      ),
-      tabs: const [
-        Tab(text: "Tab 1"),
-        Tab(text: "Tab 2"),
-        Tab(text: "Tab 3"),
-      ],
-    );
-  }
-
-  Widget _tabBarView() {
-    return TabBarView(
-      controller: tabController,
-      children: [
-        SingleChildScrollView(
-          child: Container(
-            color: Colors.red,
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                const Text(
-                  "Page 1",
-                  style: TextStyle(fontSize: 40, color: Colors.white),
-                ),
-                SizedBox(height: 500,),
-                const Text(
-                  "Page 1",
-                  style: TextStyle(fontSize: 40, color: Colors.white),
-                ),
-                SizedBox(height: 500,),
-              ],
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.blue,
-          alignment: Alignment.center,
-          child: const Text(
-            "Page 2",
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-        ),
-        Container(
-          color: Colors.green,
-          alignment: Alignment.center,
-          child: const Text(
-            "Page 3",
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _pageMoveButton() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            /// TabBar는 애니메이션 없이 바로 이동하지만
-            /// TabBarView는 화면 전환 애니메이션 발생
-            tabController.index = 2;
-          },
-          child: const Text("페이지 이동"),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            /// TabBar와 TabBarView 화면 전환 애니메이션 발생
-            tabController.animateTo(2);
-          },
-          child: const Text("페이지 이동(애니메이션)"),
-        ),
-      ],
-    );
-  }
-
   PreferredSize _buildTabBar() {
     return PreferredSize(
-      preferredSize: const Size.fromHeight(64),
+      preferredSize: const Size.fromHeight(50),
       child: Stack(
-        // alignment: Alignment.bottomLeft,
+        // alignment: Alignment.center,
         children: [
           Align(
-            alignment: Alignment.bottomCenter,
+            // alignment: Alignment.center,
             child: Container(
               // margin: const EdgeInsets.fromLTRB(0, 0, 0, 1),
-              color: Colors.yellow,
+              // color: Colors.yellow,
               width: double.infinity,
               height: 1,
             ),
           ),
           TabBar(
             isScrollable: true,
-            // labelStyle: style17sb.copyWith(letterSpacing: -0.2),
-            // unselectedLabelStyle: style17sb.copyWith(letterSpacing: -0.2),
-            // labelColor: WavyColors.grey10,
-            // unselectedLabelColor: WavyColors.grey7,
-            dividerHeight: 0,
-            // indicatorColor: WavyColors.grey9,
-            indicatorWeight: 3,
+            dividerHeight: 2,
+            indicatorWeight: 2,
             indicator: UnderlineTabIndicator(
               borderRadius: BorderRadius.circular(1.5),
               borderSide: const BorderSide(
@@ -232,8 +113,20 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
               ),
             ),
             tabs: const [
-              Center(child: Text('here1')),
-              Center(child: Text('here2')),
+              SizedBox(
+                width: 60,
+                height: 48,
+                child: Center(
+                  child: Text('here1',),
+                ),
+              ),
+              SizedBox(
+                width: 60,
+                height: 48,
+                child: Center(
+                  child: Text('here2',),
+                ),
+              ),
             ],
           ),
         ],
